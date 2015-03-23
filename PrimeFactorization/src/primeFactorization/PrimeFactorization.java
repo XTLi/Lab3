87c90cdf8d000f764adf04dd0f709bcf86238a12
XTLi <xtli1689@163.com>
2015-03-23 18:32:56 -0400

    Test that the generated list for 1, Add test_1

---
+++ PrimeFactorization/src/primeFactorization/PrimeFactorization.java
@@ -0,0 +1,12 @@
+package primeFactorization;
+
+import java.util.List;
+
+public class PrimeFactorization {
+
+	public static List<Integer> generate(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- PrimeFactorization/src/primeFactorization/PrimeFactorizationTest.java
+++ PrimeFactorization/src/primeFactorization/PrimeFactorizationTest.java
@@ -2,13 +2,22 @@ package primeFactorization;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PrimeFactorizationTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private List<Integer> list() {
+		// TODO Auto-generated method stub
+		return Arrays.asList();
 	}
+	
+	@Test
+	public void test_1() {
+		assertEquals(list(), PrimeFactorization.generate(1));
+	}
+
 
 }
