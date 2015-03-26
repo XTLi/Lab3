a0af7b8186d95bed1417e7d3db675b157da1970f
XTLi <xtli1689@163.com>
2015-03-25 20:00:38 -0400

    Task5: test input 1

---
+++ PrimeGeneration/src/primeGeneration/PrimeGeneration.java
@@ -0,0 +1,12 @@
+package primeGeneration;
+
+import java.util.List;
+
+public class PrimeGeneration {
+
+	public static List<Integer> generate(Integer inputNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
